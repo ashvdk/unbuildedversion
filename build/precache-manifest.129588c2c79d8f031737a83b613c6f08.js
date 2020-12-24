fad9b8d5e8aac1c531317b0f7ef87bea83552513
ashwin rao <ashwinvdrk@gmail.com>
2020-12-25 00:23:04 +0530

    added homepage to ashvdk.github.io/shoppingcart

---
+++ build/precache-manifest.129588c2c79d8f031737a83b613c6f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e737b3ebe09f25655224c5b26e9b2f43",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "78ef24c116b7b19e127d",
+    "url": "/shoppingcart/static/css/main.ce59ce65.chunk.css"
+  },
+  {
+    "revision": "67ab6b770579e8281d74",
+    "url": "/shoppingcart/static/js/2.b6ab210e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingcart/static/js/2.b6ab210e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ef24c116b7b19e127d",
+    "url": "/shoppingcart/static/js/main.a12f834a.chunk.js"
+  },
+  {
+    "revision": "ba0004ddcedac5112567",
+    "url": "/shoppingcart/static/js/runtime-main.93a3f646.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/shoppingcart/static/media/logo.ee7cd8ed.svg"
+  }
+]);
